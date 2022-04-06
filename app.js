d99658fa4a5d92947215e2da6ae3b8cafec64132
DemNeg <basingoudembele@gmail.com>
2022-04-06 07:34:56 +0000

    UPDATE APP.JS
    - IMPORT MONGOOSE LIBRARY AND SETUP THE CONNECTION
    
    0#

--- app.js
+++ app.js
@@ -1,54 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var auditLogsRouter = require('./routes/auditLogs');
-var customersRouter = require('./routes/customers');
-var invoicesRouter = require('./routes/invoices');
-var loansRouter = require('./routes/loans');
-var paymentsRouter = require('./routes/payments');
-var settingsRouter = require('./routes/settings');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var auditLogsRouter = require("./routes/auditLogs");
+var customersRouter = require("./routes/customers");
+var invoicesRouter = require("./routes/invoices");
+var loansRouter = require("./routes/loans");
+var paymentsRouter = require("./routes/payments");
+var settingsRouter = require("./routes/settings");
+
+var mongoose = require("mongoose");
+db_url = require("./properties").DB_URL;
+mongoose.connect(db_url);
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to MongoDB database via mongoose");
+});
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/auditLogs', auditLogsRouter);
-app.use('/customers', customersRouter);
-app.use('/invoices', invoicesRouter);
-app.use('/loans', loansRouter);
-app.use('/payments', paymentsRouter);
-app.use('/settings', settingsRouter);
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/auditLogs", auditLogsRouter);
+app.use("/customers", customersRouter);
+app.use("/invoices", invoicesRouter);
+app.use("/loans", loansRouter);
+app.use("/payments", paymentsRouter);
+app.use("/settings", settingsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
