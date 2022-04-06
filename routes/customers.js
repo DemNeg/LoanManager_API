c71622ae6f1d58da98d7576b78993d92d1cb98a5
DemNeg <basingoudembele@gmail.com>
2022-04-06 07:39:17 +0000

    UPDATE CUSTOMERS MODULE
    - Create a squeleton for CRUD opertions

--- routes/customers.js
+++ routes/customers.js
@@ -10,9 +10,29 @@ var router = express.Router();
 
 const customerModel= require('../models/customers.model');
 
-/* GET customers listing. */
+/* GET all customers. */
 router.get('/', function(req, res, next) {
   res.send('respond with customers ...');
 });
 
+/* CREATE new customer. */
+router.post('/add', function(req, res, next) {
+  res.send('respond with customers ...');
+});
+
+/* UPDATE an existing customer. */
+router.update('/update', function(req, res, next) {
+  res.send('respond with customers ...');
+});
+
+
+/* DELETE existing customer. */
+router.get('/delete', function(req, res, next) {
+  res.send('respond with customers ...');
+});
+
+/* SEARCH existing customer. */
+router.get('/search   ', function(req, res, next) {
+  res.send('respond with customers ...');
+});
 module.exports = router;
